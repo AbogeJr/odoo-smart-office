1d973882d44f866acf09990c08879fb60acb2864
AbogeJr <junioraboge@gmail.com>
2023-05-11 11:05:37 +0300

    fix: reservation compute function

--- models/reservation_model.py
+++ models/reservation_model.py
@@ -1,6 +1,7 @@
-from odoo import models, fields, api
+from odoo import models, fields, api, exceptions
 
 
+# from odoo.exceptions import UserError, ValidationError
 class ReservationModel(models.Model):
     _name = "reservation.model"
     _description = "The reservation model"
@@ -13,12 +14,18 @@ class ReservationModel(models.Model):
     approved = fields.Boolean(default=False)
     start_date = fields.Date(string="Start Date", required=True)
     end_date = fields.Date(string="End Date", required=True)
-    cost = fields.Float(string="Cost", compute="_compute_cost", store=True)
-    days = fields.Integer(string="Days", compute="_compute_cost", store=True)
+    cost = fields.Float(string="Cost", compute="_compute_cost_days", store=True)
+    days = fields.Integer(string="Days", compute="_compute_cost_days", store=True)
 
     @api.depends("office_id", "start_date", "end_date")
-    def _compute_cost(self):
+    def _compute_cost_days(self):
         for reservation in self:
-            days = (reservation.end_date - reservation.start_date).days
-            reservation.days = days
-            reservation.cost = reservation.office_id.daily_rate * days
+            if reservation.start_date and reservation.end_date:
+                if reservation.start_date > reservation.end_date:
+                    raise exceptions.ValidationError(
+                        "Start date must be before end date"
+                    )
+                days = (reservation.end_date - reservation.start_date).days
+                # days = diff.days
+                reservation.days = days
+                reservation.cost = reservation.office_id.daily_rate * days
